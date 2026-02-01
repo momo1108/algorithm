0c4f2aed7f8047e1a2632cb124c286924d77a9bd
momo1108 <momo1108@daum.net>
2026-02-01 20:28:28 +0900

    릿코드 문제 풀이 - 295 : 힙으로 구현 완료

--- js/LEETCODE/LEETCODE295.js
+++ js/LEETCODE/LEETCODE295.js
@@ -1,9 +1,10 @@
+// 최대/최소 힙을 구현하는 클래스
 class Heap {
     constructor(type = "MAX"){
         if (!["MAX", "MIN"].includes(type.toUpperCase())) {
             throw new Error("Parameter type must be 'MAX' or 'MIN'.");
         }
-        this.type = type;
+        this.type = type.toUpperCase();
         this.heapArray = [];
     }
 
@@ -23,6 +24,7 @@ class Heap {
         return root;
     }
 
+    // 부모와 자식 노드를 교환해야 하는지 판단 (MaxHeap: 부모가 더 작으면 교환, MinHeap: 부모가 더 크면 교환)
     shouldSwap(parent, child) {
         const isMaxHeap = this.type === "MAX";
         return isMaxHeap ? 
@@ -36,12 +38,13 @@ class Heap {
         this.heapArray[node2] = temp;
     }
 
+    // 새로운 원소를 삽입한 후 힙 속성을 유지하기 위해 상향 조정
     heapifyUp() {
         let child = this.heapArray.length - 1;
         let parent;
 
         while (child > 0) {
-            parent = Math.ceil(child / 2) - 1;
+            parent = Math.floor((child - 1) / 2);
 
             if (this.shouldSwap(parent, child)) {
                 this.swapHeapArrayElement(parent, child);
@@ -50,31 +53,30 @@ class Heap {
         }
     }
 
+    // 루트 원소를 제거한 후 힙 속성을 유지하기 위해 하향 조정
     heapifyDown() {
+        let heapSize = this.heapArray.length;
         let parent = 0;
         let leftChild, rightChild, swappingChild;
         
-        while (parent * 2 + 1 < this.heapArray.length) {
+        while (parent * 2 + 1 < heapSize) {
             leftChild = parent * 2 + 1;
             rightChild = parent * 2 + 2;
+            swappingChild = leftChild;
 
-            // console.log(parent, leftChild, rightChild);
-
-            if (this.heapArray[rightChild]) {
+            // 오른쪽 자식이 존재하면 두 자식 중 교환할 노드 선택
+            if (rightChild < heapSize) {
                 const isMax = this.type === "MAX";
-                const isLeft = isMax ? 
+                const pickLeft = isMax ? 
                 this.heapArray[leftChild] >= this.heapArray[rightChild] :
                 this.heapArray[leftChild] <= this.heapArray[rightChild]
-                swappingChild = isLeft ? leftChild : rightChild;
-            } else swappingChild = leftChild;
-            
-            if (this.shouldSwap(parent, swappingChild)) {
-                this.swapHeapArrayElement(parent, swappingChild);
-                parent = swappingChild;
-                continue;
+                swappingChild = pickLeft ? leftChild : rightChild;
             }
+            
+            if (!this.shouldSwap(parent, swappingChild)) break;
 
-            break;
+            this.swapHeapArrayElement(parent, swappingChild);
+            parent = swappingChild;
         }
     }
 
@@ -91,6 +93,9 @@ class Heap {
     }
 }
 
+// 중앙값을 효율적으로 찾기 위해 두 개의 힙을 사용
+// maxHeap: 작은 절반의 값들을 저장 (최댓값이 중앙값 후보)
+// minHeap: 큰 절반의 값들을 저장 (최솟값이 중앙값 후보)
 var MedianFinder = function() {
     this.maxHeap = new Heap("MAX");
     this.minHeap = new Heap("MIN");
@@ -115,22 +120,24 @@ MedianFinder.prototype.addNum = function(num) {
         return;
     }
 
-    // 1. 개수 동일
+    // 1. 개수 동일: maxHeap에 하나 더 추가하도록 조정
     if (this.maxHeap.size() === this.minHeap.size()) {
         const higherMid = this.minHeap.peek();
 
         if (num <= higherMid) this.maxHeap.insert(num);
         else {
+            // num이 크면 minHeap의 최솟값을 maxHeap으로 이동하고 num을 minHeap에 추가
             this.maxHeap.insert(this.minHeap.pop());
             this.minHeap.insert(num);
         }
     }
-    // 2. maxHeap 이 1개 더 많은 경우
+    // 2. maxHeap이 1개 더 많은 경우: 두 힙의 크기를 동일하게 만듦
     else if (this.maxHeap.size() > this.minHeap.size()) {
         const lowerMid = this.maxHeap.peek();
 
         if (lowerMid <= num) this.minHeap.insert(num);
         else {
+            // num이 작으면 maxHeap의 최댓값을 minHeap으로 이동하고 num을 maxHeap에 추가
             const popped = this.maxHeap.pop();
             this.minHeap.insert(popped);
             this.maxHeap.insert(num);
@@ -144,19 +151,10 @@ MedianFinder.prototype.addNum = function(num) {
 MedianFinder.prototype.findMedian = function() {
     const isEven = this.maxHeap.size() === this.minHeap.size();
 
+    // 짝수개: 두 힙의 최상위 값의 평균, 홀수개: maxHeap의 최댓값
     if (isEven) return (this.maxHeap.peek() + this.minHeap.peek()) / 2;
     else return this.maxHeap.peek();
 };
-
-// const mh = new Heap(type='MIN');
-// mh.insert(6)
-// mh.insert(10)
-// mh.insert(6)
-// mh.insert(6)
-// mh.insert(2)
-// mh.insert(9)
-// mh.insert(5)
-// console.log(mh)
 
 /** 
  * Your MedianFinder object will be instantiated and called as such:
@@ -165,25 +163,3 @@ MedianFinder.prototype.findMedian = function() {
  * var param_2 = obj.findMedian()
  * [[],[12],[],[10],[],[13],[],[11],[],[5],[],[15],[],[1],[],[11],[],[6],[],[17],[],[14],[],[8],[],[17],[],[6],[],[4],[],[16],[],[8],[],[10],[],[2],[],[12],[],[0],[]]
  */
-var obj = new MedianFinder()
-obj.addNum(12)
-obj.addNum(10)
-obj.addNum(13)
-obj.addNum(11)
-obj.addNum(5)
-obj.addNum(15)
-obj.addNum(1)
-obj.addNum(11)
-obj.addNum(6)
-obj.addNum(17)
-obj.addNum(14)
-obj.addNum(8)
-obj.addNum(17)
-obj.addNum(6)
-obj.addNum(4)
-obj.addNum(16)
-obj.addNum(8)
-obj.addNum(10)
-obj.addNum(2)
-obj.addNum(12)
-obj.addNum(0)
