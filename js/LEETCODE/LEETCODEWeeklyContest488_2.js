d5784688eea4015e2af824d9b4b0358cd9b3cd8f
momo1108 <momo1108@daum.net>
2026-02-11 15:55:55 +0900

    릿코드 문제 풀이 - 위클리 컨테스트 488   - 2 : 스택   - 3 : 투 포인터, 모노토닉 덱

--- js/LEETCODE/LEETCODEWeeklyContest488_2.js
+++ js/LEETCODE/LEETCODEWeeklyContest488_2.js
@@ -6,30 +6,30 @@
  */
 
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * 인접한 동일한 요소들을 반복적으로 병합하는 함수
+ * 스택을 사용하여 왼쪽부터 순회하며, 스택의 top과 현재 값이 같으면 병합
+ * 
+ * @param {number[]} nums - 입력 정수 배열
+ * @return {number[]} - 병합 완료 후 최종 배열
  */
 var mergeAdjacent = function(nums) {
-    let hasEqual;
+    const stack = [];
 
-    while(true) {
-        hasEqual = false;
-
-        let current, next;
-        for (let i = 0; i < nums.length - 1; i++) {
-            current = nums[i];
-            next = nums[i + 1];
-
-            if (current === next) {
-                hasEqual = true;
-                nums.splice(i, 2, current * 2);
-                break;
-            }
+    let currentValue;
+    for (let i = 0; i < nums.length; i++) {
+        currentValue = nums[i];
+        
+        // 스택의 top과 현재 값이 같으면 계속 병합 (2배씩 증가)
+        while (stack.length && stack[stack.length - 1] === currentValue) {
+            currentValue *= 2;  // 두 값을 합침 (동일한 값이므로 2배)
+            stack.pop();        // 스택에서 병합된 값 제거
         }
-
-        if (hasEqual) continue;
-        else break;
+        
+        // 병합된 최종 값을 스택에 추가
+        stack.push(currentValue);
     }
 
-    return nums;
+    return stack;
 };
+
+mergeAdjacent([3, 7, 5])
--- js/LEETCODE/LEETCODEWeeklyContest488_3.js
+++ js/LEETCODE/LEETCODEWeeklyContest488_3.js
@@ -9,36 +9,64 @@
  */
 
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
+ * 슬라이딩 윈도우와 모노토닉 덱을 사용하여 k 이하의 cost를 가진 부분배열 개수를 계산
+ * 
+ * 알고리즘:
+ * 1. 두 개의 덱으로 구간의 최솟값과 최댓값을 O(1)에 추적
+ * 2. 오른쪽 포인터를 확장하며 cost가 k를 초과하면 왼쪽 포인터를 이동
+ * 3. 각 right 위치에서 [left, right] 범위의 모든 부분배열이 유효 (right - left + 1개)
+ * 
+ * @param {number[]} nums - 입력 정수 배열
+ * @param {number} k - cost의 최댓값
+ * @return {number} - k 이하의 cost를 가진 부분배열의 개수
  */
 var countSubarrays = function(nums, k) {
     let answer = 0;
-
-    for (let left = 0; left < nums.length; left++) {
-        let right = left;
-        let min = nums[left];
-        let max = nums[left];
-        let cost;
-
-        while (true) {
-            cost = (max - min) * (right - left + 1);
-            if (cost > k) {
-                right--;
-                break;
-            } else if (right === nums.length - 1) break;
-            else {
-                right++;
-                min = Math.min(min, nums[right]);
-                max = Math.max(max, nums[right]);
-            }
+    
+    // 모노토닉 덱: 최솟값과 최댓값의 인덱스를 추적
+    const minDeque = [];  // 최솟값을 위한 증가 덱
+    const maxDeque = [];  // 최댓값을 위한 감소 덱
+    let minDequeHead = 0; // 헤드 포인터로 pollFirst 를 대체
+    let maxDequeHead = 0; // 헤드 포인터로 pollFirst 를 대체
+    
+    let cost = 0;
+    let left = 0;
+    
+    for (let right = 0; right < nums.length; right++) {
+        // 최솟값 덱 유지: 현재 값보다 큰 값들을 제거하여 증가 순서 유지
+        while (minDeque.length > minDequeHead && 
+               nums[minDeque[minDeque.length - 1]] > nums[right]) {
+            minDeque.pop();
         }
+        minDeque.push(right);
+        
+        // 최댓값 덱 유지: 현재 값보다 작은 값들을 제거하여 감소 순서 유지
+        while (maxDeque.length > maxDequeHead && 
+               nums[maxDeque[maxDeque.length - 1]] < nums[right]) {
+            maxDeque.pop();
+        }
+        maxDeque.push(right);
 
+        // 현재 윈도우의 cost 계산
+        cost = (nums[maxDeque[maxDequeHead]] - nums[minDeque[minDequeHead]]) * (right - left + 1);
+        
+        // cost가 k를 초과하면 왼쪽 포인터 이동
+        while (cost > k) {
+            // 왼쪽 포인터가 덱의 front를 벗어나면 head 이동
+            if (minDeque[minDequeHead] === left) minDequeHead++;
+            if (maxDeque[maxDequeHead] === left) maxDequeHead++;
+            left++;
+            
+            // cost 재계산
+            cost = (nums[maxDeque[maxDequeHead]] - nums[minDeque[minDequeHead]]) * (right - left + 1);
+        }
+        
+        // [left, right] 범위에서 right를 끝으로 하는 모든 부분배열 추가
+        // left부터 right까지 시작점을 선택할 수 있으므로 (right - left + 1)개
         answer += (right - left + 1);
     }
 
     return answer;
 };
 
-console.log(countSubarrays([1, 2, 3], 0));
+console.log(countSubarrays([1,3,2], 4));
